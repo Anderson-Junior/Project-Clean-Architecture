f2679a705ea2216773c8248ae9ab4ebbc85ef77a
Anderson-Junior <ander.lemos.jr@gmail.com>
2021-05-10 11:35:45 -0300

    implementing the infrastructure layer, creating the context, configuring the entities using Fluent API

---
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+
+            builder.HasData(
+                new Category(1, "school supplies"),
+                new Category(2, "electronics"),
+                new Category(3, "accessories")
+            );
+        }
+    }
+}
