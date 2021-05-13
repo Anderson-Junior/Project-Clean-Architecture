1d135df7c4e361f17bc53d38006bfdc0e92b115c
Anderson-Junior <ander.lemos.jr@gmail.com>
2021-05-13 16:08:55 -0300

    Creating DTOs and mappings

---
+++ CleanArchMvc.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,40 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Name is required")]
+        [MinLength(3)]
+        [MaxLength(50)]
+        public string Name { get; private set; }
+
+        [Required(ErrorMessage = "The Description is required")]
+        [MinLength(5)]
+        [MaxLength(200)]
+        public string Description { get; private set; }
+
+        [Required(ErrorMessage = "The Price is required")]
+        public decimal Price { get; private set; }
+
+        [Required(ErrorMessage = "The Stock is required")]
+        public int Stock { get; private set; }
+
+        [MaxLength(250)]
+        [DisplayName("Product Image")]
+        public string Image { get; private set; }
+
+        [DisplayName("Categories")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
